45a5ab3a52cdb1533553d7fdacd4a675f1697e87
Arseny Pochinka <Senya.pochinka@yandex.ru>
2021-12-01 19:34:45 +0300

    Some changes for schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,62 +1,76 @@
 CREATE TABLE IF NOT EXISTS Organization (
-    id        INTEGER COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
+    id        INTEGER COMMENT 'Unique organization identifier' PRIMARY KEY AUTO_INCREMENT,
     version   INTEGER NOT NULL COMMENT 'Service field hibernate',
-    name      VARCHAR(50) NOT NULL,
-    full_name VARCHAR(50) NOT NULL,
+    name      VARCHAR(50) NOT NULL COMMENT 'name',
+    full_name VARCHAR(50) NOT NULL COMMENT 'full name',
     inn       VARCHAR(12) NOT NULL COMMENT 'Taxpayer Identification Number',
     kpp       VARCHAR(9) NOT NULL COMMENT 'Code of the reason for registration',
-    address   VARCHAR(50) NOT NULL,
-    phone     VARCHAR(16),
-    is_active BIT COMMENT '1 is true, 0 is false'
+    address   VARCHAR(50) NOT NULL COMMENT 'address',
+    phone     VARCHAR(16) COMMENT 'phone',
+    is_active BIT COMMENT 'is active = 1 is true, 0 is false'
 );
+
+COMMENT ON TABLE Organization IS 'Organization';
 
 CREATE TABLE IF NOT EXISTS Office (
-    id              INTEGER COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
+    id              INTEGER COMMENT 'Unique office identifier' PRIMARY KEY AUTO_INCREMENT,
     version         INTEGER NOT NULL COMMENT 'Service field hibernate',
-    organization_id INTEGER NOT NULL,
-    name            VARCHAR(50) NOT NULL,
-    address         VARCHAR(50) NOT NULL,
-    phone           VARCHAR(16),
-    is_active       BIT COMMENT '1 is true, 0 is false'
+    organization_id INTEGER NOT NULL COMMENT 'unique organization identifier',
+    name            VARCHAR(50) NOT NULL COMMENT 'name',
+    address         VARCHAR(50) NOT NULL COMMENT 'address',
+    phone           VARCHAR(16) COMMENT 'phone',
+    is_active       BIT COMMENT 'is active = 1 is true, 0 is false'
 );
+
+COMMENT ON TABLE Office IS 'Office';
 
 CREATE TABLE IF NOT EXISTS Person (
-    id            INTEGER COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
+    id            INTEGER COMMENT 'Unique person identifier' PRIMARY KEY AUTO_INCREMENT,
     version       INTEGER NOT NULL COMMENT 'Service field hibernate',
-    office_id     INTEGER NOT NULL,
-    first_name    VARCHAR(50) NOT NULL,
-    second_name   VARCHAR(50),
-    middle_name   VARCHAR(50),
-    post          VARCHAR(50) NOT NULL,
-    phone         VARCHAR(16),
-    is_identified BIT COMMENT '1 is true, 0 is false'
+    office_id     INTEGER NOT NULL COMMENT 'Unique office identifier',
+    first_name    VARCHAR(50) NOT NULL COMMENT 'firstname',
+    second_name   VARCHAR(50) COMMENT 'second name',
+    middle_name   VARCHAR(50) COMMENT 'middle name',
+    post          VARCHAR(50) NOT NULL COMMENT 'post',
+    phone         VARCHAR(16) COMMENT 'phone',
+    is_identified BIT COMMENT 'is identified = 1 is true, 0 is false'
 );
+
+COMMENT ON TABLE Person IS 'Person';
 
 CREATE TABLE IF NOT EXISTS Doc (
-    person_id   INTEGER NOT NULL PRIMARY KEY,
+    person_id   INTEGER NOT NULL COMMENT 'Unique person identifier' PRIMARY KEY,
     version     INTEGER NOT NULL COMMENT 'Service field hibernate',
-    doc_type_id INTEGER NOT NULL,
-    doc_number  VARCHAR(12) NOT NULL,
-    doc_date    VARCHAR(10) NOT NULL
+    doc_type_id INTEGER NOT NULL COMMENT 'Unique type of doc identifier',
+    doc_number  VARCHAR(12) NOT NULL COMMENT 'number of doc',
+    doc_date    VARCHAR(10) NOT NULL COMMENT 'date of doc'
 );
 
+COMMENT ON TABLE Doc IS 'Doc';
+
 CREATE TABLE IF NOT EXISTS TypeDoc (
-    id   INTEGER COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    code VARCHAR(10) NOT NULL
+    id   INTEGER COMMENT 'Unique type of doc identifier' PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL COMMENT 'name',
+    code VARCHAR(10) NOT NULL COMMENT 'code'
 );
+
+COMMENT ON TABLE TypeDoc IS 'Type of doc';
 
 CREATE TABLE IF NOT EXISTS Country (
     id      INTEGER COMMENT 'Unique identifier' PRIMARY KEY AUTO_INCREMENT,
     version INTEGER NOT NULL COMMENT 'Service field hibernate',
-    name    VARCHAR(50) NOT NULL,
-    code    VARCHAR(10) NOT NULL
+    name    VARCHAR(50) NOT NULL COMMENT 'name',
+    code    VARCHAR(10) NOT NULL COMMENT 'code'
 );
 
+COMMENT ON TABLE Country IS 'Country';
+
 CREATE TABLE IF NOT EXISTS Person_Country (
-    person_id  INTEGER COMMENT 'Unique identifier person',
-    country_id INTEGER COMMENT 'Unique identifier country'
+    person_id  INTEGER COMMENT 'Unique person identifier',
+    country_id INTEGER COMMENT 'Unique country identifier'
 );
+
+COMMENT ON TABLE Person_Country IS 'join-table for connecting a person and a country';
 
 ALTER TABLE Office ADD FOREIGN KEY (organization_id) REFERENCES Organization(id);
 
